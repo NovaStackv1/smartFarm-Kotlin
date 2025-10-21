ba5515384557040c24015c48fdf3e5ac97de159f
Devkorrir <kipyegonaldo@proton.me>
2025-10-22 02:53:40 +0300

    setup hilt dagger and more expectected libraries

--- app/src/main/java/com/example/smartfarm/activity/MainActivity.kt
+++ app/src/main/java/com/example/smartfarm/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.smartfarm
+package com.example.smartfarm.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartfarm.ui.theme.SmartFarmTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
