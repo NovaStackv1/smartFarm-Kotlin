b81c81bfad01a5fddf4e3affbbc98c17d9b10a5b
Devkorrir <kipyegonaldo@proton.me>
2025-10-24 10:30:54 +0300

    aplly dark theme to whole app

--- app/src/main/java/com/example/smartfarm/activity/MainActivity.kt
+++ app/src/main/java/com/example/smartfarm/activity/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.smartfarm.activity
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,12 +19,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.smartfarm.navigation.NavGraph
 import com.example.smartfarm.ui.features.auth.view.LoginScreen
+import com.example.smartfarm.ui.features.settings.presentation.viewModel.AppTheme
+import com.example.smartfarm.ui.features.settings.presentation.viewModel.SettingsViewModel
 import com.example.smartfarm.ui.theme.NegativeRed
 import com.example.smartfarm.ui.theme.PositiveGreen
 import com.example.smartfarm.ui.theme.SmartFarmTheme
@@ -29,14 +37,31 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
 
             val navController = rememberNavController()
+            val settingsViewModel: SettingsViewModel = hiltViewModel()
 
-            SmartFarmTheme {
+            val uiState by settingsViewModel.uiState.collectAsState()
+            val themePreference = uiState.theme
+
+            val useDarkTheme = when (themePreference) {
+                AppTheme.DARK -> true
+                AppTheme.LIGHT -> false
+                AppTheme.SYSTEM -> isSystemInDarkTheme() // Check the system setting
+            }
+
+
+
+
+            SmartFarmTheme (
+                darkTheme = useDarkTheme,
+                dynamicColor = false
+            ){
                 NavGraph(
                     navController = navController
                 )
